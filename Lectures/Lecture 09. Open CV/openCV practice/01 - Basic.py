71bae3f49762521781cd4ffb451429d6bebb9e81
Kirill Svyatov <k.svyatov@gmail.com>
2017-08-24 08:50:12 +0400

    opencv lecture added

---
+++ Lectures/Lecture 09. Open CV/openCV practice/01 - Basic.py
@@ -0,0 +1,30 @@
+import cv2
+
+# load image
+source_image = cv2.imread('lena.bmp')
+# get image matrix shape
+height, width, channel = source_image.shape
+print 'SOURCE IMAGE SHAPE'
+print 'height', height
+print 'width', width
+print 'channel', channel
+
+# convert color from RGB to GRAY
+# RGB is 3-dim array, GRAY is 2-dim array (channel is none)
+result_image = cv2.cvtColor(source_image, cv2.COLOR_RGB2GRAY)
+height, width = result_image.shape
+print 'GRAY IMAGE SHAPE'
+print 'height', height
+print 'width', width
+
+# drawing images in window
+# need to set win name and image for drawing
+cv2.imshow('SOURCE', source_image)
+cv2.imshow('RESULT', result_image)
+
+# waiting windows closing
+cv2.waitKey(0)
+
+
+# save reslut into new file and change file format
+cv2.imwrite('result.png', result_image)
