5acd9847cc7ca7a0b3a8f726a338e5a53dd28f5a
Kirill Svyatov <9466203+ksvyatov@users.noreply.github.com>
2021-10-22 07:13:39 +0400

    lec01 en updated

--- ml_course_en/lecture02. Basics of python/test.py
+++ ml_course_en/lecture02. Basics of python/test.py
@@ -1,18 +1,2 @@
-import random as r
-import numpy as np
-import matplotlib.pyplot as plt
-import math
-x = np.linspace(0, 5, 50)
-y1 = [math.sin(i) + r.uniform(-0.2, 0.2) for i in x]
-y2 = [math.sin(i) for i in x]
-#print(x, y)
-
-plt.subplot(2, 1, 1)
-plt.plot(x, y1, 'ro')
-plt.xlabel("x")
-plt.ylabel("y1")
-plt.subplot(2, 1, 2)
-plt.plot(x, y2, 'b--')
-
-
-
+#!/usr/bin/env python3
+print('hello world')
